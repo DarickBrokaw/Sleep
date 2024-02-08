eea9ff21c49029947acae2546da59b9283df67f9
Darick Brokaw <93608701+DarickBrokaw@users.noreply.github.com>
2024-02-08 09:53:48 -0500

    first upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    // Import the SetSuspendState function from powrprof.dll
+    [DllImport("powrprof.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+    public static extern bool SetSuspendState(bool hibernate, bool forceCritical, bool disableWakeEvent);
+
+    static void Main()
+    {
+        // Put the computer to sleep
+        // hibernate = false (sleep mode), forceCritical = false, disableWakeEvent = false
+        SetSuspendState(false, false, false);
+    }
+}
